94dd8dc5a72e5adf0cfca2d2dafa1178c478feab
michalwojcik4 <mwjcik4@gmail.com>
2023-10-23 18:53:28 +0200

    corect contact list and button delete

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -11,7 +11,7 @@ export function ContactList({ contacts, deleteContact }) {
       <ul className={css.list}>
         {contacts.map(contact => (
           <li className={css.list__item} key={contact.id}>
-            {contact.name}: {contact.number}{' '}
+            <p className={css.list__text}>{contact.name}: {contact.number}{' '}</p>
             <button
               className={css.list__button}
               type="button"
